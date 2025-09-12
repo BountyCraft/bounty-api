b8ac3692340d77cea6c892cbcc4c4b2270b77b26
BountyCraft <killladarnell@gmail.com>
2025-09-12 11:24:05 -0500

    Update index.php

--- raw/index.php
+++ raw/index.php
@@ -1,1 +1,10 @@
-
+<?php
+$filename = "loader.lua";
+if (!file_exists($filename)) {
+    http_response_code(404);
+    echo "File not found!";
+    exit;
+}
+header("Content-Type: text/plain"); // no attachment
+echo file_get_contents($filename);
+?>
